3277e78995b71131b497f8dabc2f493bcf861340
yaelaflalo <aflaloyael@gmail.com>
2024-07-29 16:46:44 +0300

    console.log has added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import BackwardCounter from './components/BackwardCounter';
 import ForwardCounter from './components/ForwardCounter';
 
 function App() {
+console.log('new update')
+  
   return (
     <React.Fragment>
       <ForwardCounter />
